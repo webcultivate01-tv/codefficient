ddf345aa0b496d8c3be7220e8be65ba701102a9d
its1om1dhage <dhagesoham513@gmail.com>
2025-09-22 19:07:14 +0530

    Changed the Opacity of the nav bar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full bg-black/40 backdrop-blur-md z-50">
+      <nav className="fixed top-0 left-0 w-full bg-black/90 backdrop-blur-md z-50">
 
         <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
           {/* Logo */}
