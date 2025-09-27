b14eec038d8c9b8cbe79547671c708fee0036779
Tejas mehar <tejas mehar7@gmail.com>
2025-09-27 17:15:44 +0530

    Added Hero compoenet in the Contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import EducationalContactPage from "../components/contact/EducationalContactPage";
 import AnimatedCards from "../components/contact/AnimatedCards";
+import Hero from "../components/contact/ContactHero";
 
 export default function Contact() {
   return (
     <div className="bg-black">
+      <Hero/>
       <EducationalContactPage />
       <AnimatedCards/>
     </div>
